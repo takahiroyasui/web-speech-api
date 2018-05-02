0803c50aab3cf07214d0b7c2c625ad92c3b3e4f8
takahiroyasui <takahiro@uniba.jp>
2018-05-02 22:25:07 +0900

    socket.io追加

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 "use strict"; 
 
-const fs    = require('fs');
+const fs = require('fs');
 const https = require('https');
 
-// express を使う
-var express = require("express");
-var app = express();
-app.use(express.static(__dirname));
+// express
+const app = require('express')();
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
 // 秘密鍵とデジタル証明書ファイルを指定
-const options = { 
+const options = {
     key : fs.readFileSync('key.pem'),
     cert: fs.readFileSync('cert.pem')
 };
 
-// サーバ起動
-https.createServer(options, app).listen(8443);
-
+// https Server
+const server = require('https').Server(options, app);
+server.listen(8443);
 console.log('自己署名証明書サーバを起動');
+
+// socket.io
+const io = require('socket.io')(server);
+io.on('connection', socket => {
+    console.log('a user connected');
+});
